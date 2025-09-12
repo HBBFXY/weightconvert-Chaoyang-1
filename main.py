3a53293e02cc8437f91e2c41d9fa4766c6efd8f3
Chaoyang-1 <2177885770@qq.com>
2025-09-12 21:56:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # 在这个文件下编写代码，题目具体要求见README.md文件
-weight=input("请输入带有符号的重量")
+weight=input()
 if weight[-2:] in ["kg"]:
     poung = (eval(weight[0:-2]))*2.2046
-    print("对应的重量为：{：。3f}磅".format(poung))
+    print("对应的英制重量为{:.3f}磅".format(poung))
 elif weight[-2:]in["pd"]:
-    kg = (eval(weight[0:-2])/2.2046)
-    print("对应的重量为:{:3f}千克".format(kg))
+    kg = (eval(weight[0:-2]))*0.4535
+    print("对应的公制重量为{:.3f}公斤".format(kg))
 else:
     print("输入错误")    
